5f4a436827d7ca58096f03dc956519790702a6d3
Román Yakimovicz <romanyaki@hotmail.com>
2025-07-08 16:59:22 -0300

    Conecta todo en la Clase

--- src/main/java/com/ryakimovicz/literalura/LiteraluraApplication.java
+++ src/main/java/com/ryakimovicz/literalura/LiteraluraApplication.java
@@ -1,6 +1,7 @@
 package com.ryakimovicz.literalura;
 
-
+import com.ryakimovicz.literalura.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
 
+	@Autowired // Inyecta el repositorio
+	private LibroRepository libroRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Principal principal = new Principal();
+		Principal principal = new Principal(libroRepository); // Pasa el repositorio a Principal
 		principal.muestraElMenu();
 	}
 }
--- src/main/java/com/ryakimovicz/literalura/Principal.java
+++ src/main/java/com/ryakimovicz/literalura/Principal.java
@@ -1,16 +1,30 @@
 package com.ryakimovicz.literalura;
 
+import com.ryakimovicz.literalura.model.Autor;
 import com.ryakimovicz.literalura.model.Datos;
+import com.ryakimovicz.literalura.model.DatosLibro;
+import com.ryakimovicz.literalura.model.Libro;
+import com.ryakimovicz.literalura.repository.LibroRepository;
 import com.ryakimovicz.literalura.service.ConsumoAPI;
 import com.ryakimovicz.literalura.service.ConvierteDatos;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
+@Service // Anotación para marcar como un servicio de Spring
 public class Principal {
     private Scanner teclado = new Scanner(System.in);
     private ConsumoAPI consumoApi = new ConsumoAPI();
     private ConvierteDatos conversor = new ConvierteDatos();
     private final String URL_BASE = "https://gutendex.com/books/";
+    private LibroRepository libroRepository; // Repositorio
+
+    // Constructor para inyectar el repositorio
+    public Principal(LibroRepository libroRepository) {
+        this.libroRepository = libroRepository;
+    }
 
     public void muestraElMenu() {
         var opcion = -1;
@@ -45,7 +59,26 @@ public class Principal {
         System.out.println("Escribe el nombre del libro que deseas buscar:");
         var nombreLibro = teclado.nextLine();
         var json = consumoApi.obtenerDatos(URL_BASE + "?search=" + nombreLibro.replace(" ", "+"));
-        var datos = conversor.obtenerDatos(json, Datos.class);
-        System.out.println(datos);
+        var datosBusqueda = conversor.obtenerDatos(json, Datos.class);
+
+        Optional<DatosLibro> libroBuscado = datosBusqueda.resultados().stream()
+                .findFirst();
+
+        if (libroBuscado.isPresent()) {
+            System.out.println("Libro Encontrado ");
+            var datosLibro = libroBuscado.get();
+
+            Libro libro = new Libro(datosLibro);
+            Autor autor = new Autor(datosLibro.autor().get(0));
+            libro.setAutor(autor);
+
+            // Guardamos en la base de datos
+            libroRepository.save(libro);
+
+            System.out.println(libro);
+
+        } else {
+            System.out.println("Libro no encontrado");
+        }
     }
 }
